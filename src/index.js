cf2d216e0442747ddb1ff81d7c7832369ba4ecfa
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-02 18:51:44 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -40,8 +40,9 @@ const accordion = document.querySelector('.accordion')
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
-function makePanel({ title, content }/* what data does the panel need? */) {
+function makePanel({ title, content } /* what data does the panel need? */) {
 
+  // I can see the arrows already
 
   // TASK 5- Instantiate all the elements needed for a panel
   const panel = null
