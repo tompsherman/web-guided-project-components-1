9fe471fd77f6d33f11e1a601d77113c693a1b3e1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-02 19:28:14 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ function makePanel({ title, content } /* what data does the panel need? */) {
   panelContent.classList.add('panel-content')
   panelButtons.classList.add('panel-buttons')
   openButton.classList.add('panel-btn-open')
-  closeButton.classList.add('panel-btn-close hide-btn')
+  closeButton.classList.add('panel-btn-close', 'hide-btn')
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
